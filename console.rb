315a99ff1e832241f32e9ce9d809f0f8ce43243d
Karl Krukow <karl.krukow@gmail.com>
2014-11-07 09:14:24 +0000

    Make the scripts a bit friendlier for mal-configed systems

--- console.rb
+++ console.rb
@@ -1,6 +1,18 @@
 #!/usr/bin/env ruby
 target = ARGV[0]
 
+unless system("bundle version")
+  puts "Can't find bundler. Check your ruby environment."
+  puts "If your using ~/.calabash then run:"
+  puts <<EOF
+
+export GEM_HOME=~/.calabash
+export GEM_PATH=~/.calabash
+export PATH="$PATH:$HOME/.calabash/bin"
+EOF
+  exit(false)
+end
+
 if target == 'android'
   exec("bundle exec calabash-android console prebuilt/Android-debug.apk")
 elsif target == 'ios'
--- run.rb
+++ run.rb
@@ -1,6 +1,18 @@
 #!/usr/bin/env ruby
 target = ARGV[0]
 
+unless system("bundle version")
+  puts "Can't find bundler. Check your ruby environment."
+  puts "If your using ~/.calabash then run:"
+  puts <<EOF
+
+export GEM_HOME=~/.calabash
+export GEM_PATH=~/.calabash
+export PATH="$PATH:$HOME/.calabash/bin"
+EOF
+  exit(false)
+end
+
 if target == 'android'
   exec("bundle exec calabash-android run prebuilt/Android-debug.apk -p android")
 elsif target == 'ios'
